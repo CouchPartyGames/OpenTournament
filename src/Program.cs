b62e70e3028990d5cba775341f4d28d6f7890bba
CouchParty Games <83233322+CouchPartyGames@users.noreply.github.com>
2023-12-06 09:18:15 -0500

    feat: add global exception handler

--- src/Program.cs
+++ src/Program.cs
@@ -1,4 +1,7 @@
+using OpenTournament.Common.Exceptions;
+
 var builder = WebApplication.CreateSlimBuilder(args);
+builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
