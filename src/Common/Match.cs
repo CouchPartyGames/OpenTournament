707572daed390ca67b96cf9d03b7fa88cd91abab
CouchParty Games <83233322+CouchPartyGames@users.noreply.github.com>
2023-12-12 17:50:53 -0500

    chore: add fields to tournament/match

--- src/Common/Match.cs
+++ src/Common/Match.cs
@@ -2,7 +2,16 @@ namespace OpenTournament.Common;
 
 public sealed record MatchId(Guid Value);
 
+public enum MatchStatus
+{
+    Ready = 0,
+    InProgress,
+    Complete
+};
+
 public sealed class Match
 {
     public MatchId Id { get; set; }
+
+    public MatchStatus Status { get; set; } = MatchStatus.Ready;
 }
--- src/Common/Tournament.cs
+++ src/Common/Tournament.cs
@@ -1,11 +1,6 @@
-using System.ComponentModel;
-using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
 
 namespace OpenTournament.Common;
 
-
-[ComplexType]
 public sealed record TournamentId(Guid Value);
 
 public enum Status
@@ -15,14 +10,13 @@ public enum Status
     Completed
 }
 
-[ComplexType]
 public sealed class Tournament
 {
-    [Required]
     public TournamentId Id { get; set; }
-    //public Guid Id { get; set; }
-    public required string Name { get; set; }   
     
-    public Status Status { get; set; }
-    //public DateTime StartTime { get; set;  }
+    public required string Name { get; set; }
+
+    public Status Status { get; set; } = Status.Registration;
+    
+    public DateTime StartTime { get; set;  }
 }
