932352040ecb752f166f8bca0d69d49c67c9bf8e
CouchPartyGames <83233322+CouchPartyGames@users.noreply.github.com>
2024-01-02 11:24:18 -0500

    feat: add nsubstitute, opp match position testing

---
+++ tests/OpenTournament.Tests.Unit/ParticipantDrawPositionsTest.cs
@@ -0,0 +1,30 @@
+﻿namespace OpenTournament.Tests.Unit;
+
+using OpenTournament.Common.Draw.Layout;
+
+public class ParticipantDrawPositionsTest
+{
+    [Theory]
+    [InlineData(2, 1)]
+    [InlineData(4, 2)]
+    [InlineData(8, 4)]
+    [InlineData(16, 8)]
+    [InlineData(32, 16)]
+    [InlineData(64, 32)]
+    [InlineData(128, 64)]
+    public void GetDrawSize_ShouldMatch_WhenDrawSize(int size, int expectedMatchCount)
+    {
+
+        // Arrange
+        DrawSize drawSize = DrawSize.Create((DrawSize.Size) size);
+        
+        // Act
+        var positions = new ParticipantPositions(drawSize);
+        
+        // Assert
+        positions.Matches.Should()
+            .NotBeEmpty()
+            .And.HaveCount(expectedMatchCount)
+            .And.ContainItemsAssignableTo<VersusMatch>();
+    }
+}
