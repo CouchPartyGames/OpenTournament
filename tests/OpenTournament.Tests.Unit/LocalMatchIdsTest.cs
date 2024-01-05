44c8b0c34959e89c8b8ad1a004f343e38982e955
CouchPartyGames <83233322+CouchPartyGames@users.noreply.github.com>
2024-01-05 13:11:45 -0500

    fix: correct tests for 64,128

--- tests/OpenTournament.Tests.Unit/LocalMatchIdsTest.cs
+++ tests/OpenTournament.Tests.Unit/LocalMatchIdsTest.cs
@@ -4,7 +4,15 @@ namespace OpenTournament.Tests.Unit;
 
 public class LocalMatchIdsTest
 {
-    [Fact]
+    private readonly ITestOutputHelper _output;
+    
+    public LocalMatchIdsTest(ITestOutputHelper output)
+    {
+        _output = output;
+    }
+
+
+[Fact]
     public void CreateMatchIds_ShouldReturnDictionary_WhenDrawSize2()
     {
         // Arrange
@@ -82,4 +90,72 @@ public class LocalMatchIdsTest
         actual.Should().BeEquivalentTo(expected);
     }
     
+    [Fact]
+    public void CreateMatchIds_ShouldReturnDictionary_WhenDrawSize32()
+    {
+        // Arrange
+        var expected = new Dictionary<int, List<int>>()
+        {
+            { 1, Enumerable.Range(1, 16).ToList() },
+            { 2, Enumerable.Range(17, 8).ToList() },
+            { 3, Enumerable.Range(25, 4).ToList() },
+            { 4, [ 29, 30] },
+            { 5, [ 31 ] }
+        };
+        DrawSize drawSize = DrawSize.Create(DrawSize.Size.Size32);
+        
+        // Act
+        var ids = new LocalMatchIds(drawSize);
+        var actual = ids.CreateMatchIds();
+
+        // Assert
+        actual.Should().BeEquivalentTo(expected);
+    }
+    
+    [Fact]
+    public void CreateMatchIds_ShouldReturnDictionary_WhenDrawSize64()
+    {
+        // Arrange
+        var expected = new Dictionary<int, List<int>>()
+        {
+            { 1, Enumerable.Range(1, 32).ToList() },
+            { 2, Enumerable.Range(33, 16).ToList() },
+            { 3, Enumerable.Range(49, 8).ToList() },
+            { 4, [ 57, 58, 59, 60] },
+            { 5, [ 61, 62] },
+            { 6, [ 63 ] }
+        };
+        DrawSize drawSize = DrawSize.Create(DrawSize.Size.Size64);
+        
+        // Act
+        var ids = new LocalMatchIds(drawSize);
+        var actual = ids.CreateMatchIds();
+
+        // Assert
+        actual.Should().BeEquivalentTo(expected);
+    }
+    
+    [Fact]
+    public void CreateMatchIds_ShouldReturnDictionary_WhenDrawSize128()
+    {
+        // Arrange
+        var expected = new Dictionary<int, List<int>>()
+        {
+            { 1, Enumerable.Range(1, 64).ToList() },
+            { 2, Enumerable.Range(65, 32).ToList() },
+            { 3, Enumerable.Range(97, 16).ToList() },
+            { 4, Enumerable.Range(113, 8).ToList() },
+            { 5, [ 121, 122, 123, 124 ] },
+            { 6, [ 125, 126 ] },
+            { 7, [ 127 ] }
+        };
+        DrawSize drawSize = DrawSize.Create(DrawSize.Size.Size128);
+        
+        // Act
+        var ids = new LocalMatchIds(drawSize);
+        var actual = ids.CreateMatchIds();
+
+        // Assert
+        actual.Should().BeEquivalentTo(expected);
+    }
 }
