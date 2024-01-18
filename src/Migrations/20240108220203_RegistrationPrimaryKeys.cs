f1bb5494f4a161b6156d3ff6a2e5eb5a62c69b27
CouchParty Games <83233322+CouchPartyGames@users.noreply.github.com>
2024-01-18 14:23:39 -0500

    chore: add migrations

---
+++ src/Migrations/20240108220203_RegistrationPrimaryKeys.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OpenTournament.Migrations
+{
+    /// <inheritdoc />
+    public partial class RegistrationPrimaryKeys : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Registrations",
+                table: "Registrations");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Registrations",
+                table: "Registrations",
+                columns: new[] { "TournamentId", "ParticipantId" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Registrations",
+                table: "Registrations");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Registrations",
+                table: "Registrations",
+                column: "TournamentId");
+        }
+    }
+}
---
+++ src/Migrations/20240109205009_Seeding.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OpenTournament.Migrations
+{
+    /// <inheritdoc />
+    public partial class Seeding : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Participants",
+                columns: new[] { "Id", "Name" },
+                values: new object[] { new Guid("f924898c-249c-4ff3-b483-5dfe2819a66d"), "Bye" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Participants",
+                keyColumn: "Id",
+                keyValue: new Guid("f924898c-249c-4ff3-b483-5dfe2819a66d"));
+        }
+    }
+}
