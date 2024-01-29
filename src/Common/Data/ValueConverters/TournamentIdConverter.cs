d47f027abd56a5294b69e63d4cf649dc64cca0c9
CouchPartyGames <83233322+CouchPartyGames@users.noreply.github.com>
2024-01-28 20:14:55 -0500

    feat: add tournamentid converter

---
+++ src/Common/Data/ValueConverters/TournamentIdConverter.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+using OpenTournament.Common.Models;
+
+namespace OpenTournament.Common.Data.ValueConverters;
+
+
+public sealed class TournamentIdConverter : ValueConverter<TournamentId, Guid>
+{
+    public TournamentIdConverter() : base(
+        v => v.Value,
+        v => new TournamentId(v)
+    )
+    {
+    }
+}
