24f7786869b68cc58d94d9fdb56aa922fccbb7d9
CouchParty Games <83233322+CouchPartyGames@users.noreply.github.com>
2024-02-23 11:48:45 -0500

    feat: add configuration options for opentelemetry

---
+++ src/Options/OpenTelemetryOptions.cs
@@ -0,0 +1,10 @@
+namespace OpenTournament.Options;
+
+public sealed class OpenTelemetryOptions
+{
+   public const string SectionName = "OpenTelemetry";
+
+   public bool Enabled { get; init; } = false;
+   
+   public string Address { get; init; } = String.Empty;
+}
