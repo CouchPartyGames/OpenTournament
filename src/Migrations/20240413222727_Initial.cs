73d00ae18c569aa406f75e2b0f606af92e22693e
CouchPartyGames <83233322+CouchPartyGames@users.noreply.github.com>
2024-04-13 18:30:14 -0400

    chore: add migrations

---
+++ src/Migrations/20240413222727_Initial.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OpenTournament.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Outboxes",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    EventName = table.Column<string>(type: "text", nullable: false),
+                    Content = table.Column<string>(type: "text", nullable: false),
+                    Created = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    Processed = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    State = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Outboxes", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Participants",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "varchar(36)", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    Rank = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Participants", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Tournaments",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "varchar(36)", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    Status = table.Column<int>(type: "integer", nullable: false),
+                    StartTime = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    MinParticipants = table.Column<int>(type: "integer", nullable: false),
+                    MaxParticipants = table.Column<int>(type: "integer", nullable: false),
+                    DrawSize = table.Column<int>(type: "integer", nullable: true),
+                    Created = table.Column<DateTime>(type: "timestamp with time zone", nullable: false, defaultValueSql: "now()"),
+                    Completed = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    EliminationMode = table.Column<int>(type: "integer", nullable: false),
+                    RegistrationMode = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tournaments", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Registrations",
+                columns: table => new
+                {
+                    TournamentId = table.Column<Guid>(type: "uuid", nullable: false),
+                    ParticipantId = table.Column<string>(type: "varchar(36)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Registrations", x => new { x.TournamentId, x.ParticipantId });
+                    table.ForeignKey(
+                        name: "FK_Registrations_Participants_ParticipantId",
+                        column: x => x.ParticipantId,
+                        principalTable: "Participants",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Matches",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "varchar(36)", nullable: false),
+                    State = table.Column<int>(type: "integer", nullable: false),
+                    LocalMatchId = table.Column<int>(type: "integer", nullable: false),
+                    Participant1Id = table.Column<string>(type: "varchar(36)", nullable: false),
+                    Participant2Id = table.Column<string>(type: "varchar(36)", nullable: false),
+                    WinMatchId = table.Column<int>(type: "integer", nullable: true),
+                    LoseMatchId = table.Column<int>(type: "integer", nullable: false),
+                    WinnerId = table.Column<string>(type: "text", nullable: true),
+                    TournamentId = table.Column<string>(type: "varchar(36)", nullable: false),
+                    Created = table.Column<DateTime>(type: "timestamp with time zone", nullable: false, defaultValueSql: "now()"),
+                    Completed = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Matches", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Matches_Participants_Participant1Id",
+                        column: x => x.Participant1Id,
+                        principalTable: "Participants",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Matches_Participants_Participant2Id",
+                        column: x => x.Participant2Id,
+                        principalTable: "Participants",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Matches_Tournaments_TournamentId",
+                        column: x => x.TournamentId,
+                        principalTable: "Tournaments",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Participants",
+                columns: new[] { "Id", "Name", "Rank" },
+                values: new object[] { "FFFFFFFF-FFFF-FFFF-FFFF-FFFFFFFFFFFF", "Bye", 2147483647 });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Matches_Participant1Id",
+                table: "Matches",
+                column: "Participant1Id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Matches_Participant2Id",
+                table: "Matches",
+                column: "Participant2Id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Matches_TournamentId",
+                table: "Matches",
+                column: "TournamentId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Registrations_ParticipantId",
+                table: "Registrations",
+                column: "ParticipantId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Matches");
+
+            migrationBuilder.DropTable(
+                name: "Outboxes");
+
+            migrationBuilder.DropTable(
+                name: "Registrations");
+
+            migrationBuilder.DropTable(
+                name: "Tournaments");
+
+            migrationBuilder.DropTable(
+                name: "Participants");
+        }
+    }
+}
