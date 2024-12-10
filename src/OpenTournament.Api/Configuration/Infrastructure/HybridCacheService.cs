6ecabdaeff64e8492c6ed1508937c5cc58a47e91
CouchPartyGames <83233322+CouchPartyGames@users.noreply.github.com>
2024-12-09 21:38:08 -0500

    feat: add local and redis caching

--- src/OpenTournament.Api/Configuration/Infrastructure/HybridCacheService.cs
+++ src/OpenTournament.Api/Configuration/Infrastructure/HybridCacheService.cs
@@ -7,12 +7,18 @@ public static class HybridCacheService
     public static IServiceCollection AddHybridCacheServices(this IServiceCollection services, IConfiguration configuration)
     {
         services.Configure<HybridCacheOptions>(configuration.GetSection(HybridCacheOptions.SectionName));
+
+        services.AddStackExchangeRedisCache(opts =>
+        {
+            opts.Configuration = "";
+            opts.InstanceName = "";
+        });
         
         #pragma warning disable
         services.AddHybridCache(opts =>
         {
-            opts.MaximumKeyLength = 256;
-            opts.MaximumPayloadBytes = 1024;
+            opts.MaximumKeyLength = 64;
+            opts.MaximumPayloadBytes = 1024 * 1024; // 1024 * 1024 = 1MB
             opts.DefaultEntryOptions = new HybridCacheEntryOptions
             {
                 Expiration = TimeSpan.FromMinutes(10),
