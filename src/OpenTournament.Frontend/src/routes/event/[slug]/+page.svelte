43b40722cdabc75b6deea90c98af3b2e5c2e2d76
CouchParty Games <83233322+CouchPartyGames@users.noreply.github.com>
2024-12-18 16:48:50 -0500

    fix: add simple updates to frontend

---
+++ src/OpenTournament.Frontend/src/routes/event/[slug]/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+import EventBanner from "$lib/event/EventBanner.svelte";
+import { page } from '$app/stores';
+</script>
+
+<div>
+    <img src="" />
+</div>
+<h1>Event {$page.params.slug}</h1>
+<EventBanner></EventBanner>
