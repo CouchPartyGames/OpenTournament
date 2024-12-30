9f4a019834e504ec0e4f16d1137ca8c8e7da9aa2
CouchPartyGames <83233322+CouchPartyGames@users.noreply.github.com>
2024-12-29 21:26:44 -0500

    feat: add admin backend for events,competitions

---
+++ src/OpenTournament.Frontend/src/routes/admin/Competition.svelte
@@ -0,0 +1,23 @@
+﻿<fieldset class="fieldset w-xs bg-base-200 border border-base-300 p-4 rounded-box">
+
+    <legend class="fieldset-legend">Competition details</legend>
+
+    <label class="fieldset-label">Name</label>
+    <input type="text" class="input" placeholder="My awesome page" />
+    
+    <label class="fieldset-label">Game</label>
+    <input type="text" class="input" placeholder="My awesome page" />
+
+    <label class="fieldset-label">Rules</label>
+    <textarea class="textarea" placeholder="Competition rules ..."></textarea>
+
+    <label class="fieldset-label">Mode</label>
+    <input type="text" class="input" placeholder="my-awesome-page" />
+
+    <label class="fieldset-label">Visibility</label>
+    <input type="text" class="input" placeholder="Name" />
+    
+    <label class="fieldset-label">Player Cap</label>
+    <input type="text" class="input" placeholder="Name" />
+
+</fieldset>
---
+++ src/OpenTournament.Frontend/src/routes/admin/Event.svelte
@@ -0,0 +1,26 @@
+﻿<fieldset class="fieldset w-xs bg-base-200 border border-base-300 p-4 rounded-box">
+    
+    <legend class="fieldset-legend">Page details</legend>
+
+    <label class="fieldset-label">Name</label>
+    <input type="text" class="input" placeholder="My awesome page" />
+
+    <label class="fieldset-label">Description</label>
+    <input type="text" class="input" placeholder="My awesome page" />
+    
+    <label class="fieldset-label">Slug</label>
+    <input type="text" class="input" placeholder="my-awesome-page" />
+
+    <label class="fieldset-label">Location</label>
+    <input type="text" class="input" placeholder="Name" />
+    
+    <label class="fieldset-label">Visibility</label>
+    <input type="text" class="input" placeholder="Name" />
+
+    <label class="fieldset-label">Start Date</label>
+    <input type="text" class="input" placeholder="Name" />
+
+    <label class="fieldset-label">End Date</label>
+    <input type="text" class="input" placeholder="Name" />
+    
+</fieldset>
